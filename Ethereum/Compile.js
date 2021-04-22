e367c4da14eb904955d5fe8fc7359361d44738fe
Dallin Breen <dallinbreen@Dallins-MacBook-Pro.local>
2021-04-22 15:34:13 -0400

    Adding project files and compiling solidity contracts

--- Ethereum/Compile.js
+++ Ethereum/Compile.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
-const buildPath = path.resolve(__dirname, 'build');
+
+const buildPath = path.resolve(__dirname, 'Build');
 fs.removeSync(buildPath);
-const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+
+const campaignPath = path.resolve(__dirname, 'Contracts', 'Campaign.sol');
 const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;
 fs.ensureDirSync(buildPath);
+
 for (let contract in output) {
     fs.outputJsonSync(
         path.resolve(buildPath, contract.replace(':', '') + '.json'),
